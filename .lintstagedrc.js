6ebdd720b1dfffd3a5cbad351be3040c85c49868
Apollo <baotrung06092003@gmail.com>
2024-04-07 19:25:54 +0700

    feat: small change file husky

---
+++ .lintstagedrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  //   // Type check TypeScript files
+  //   "packages/**/*.(ts|tsx|js)": () => "tsc --noEmit",
+  //   // Lint then format TypeScript and JavaScript files
+  //   "packages/**/*.(ts|tsx|js)": (filenames) => [
+  //     // `eslint --fix ${filenames.join(" ")}`,
+  //     `prettier --write ${filenames.join(" ")}`,
+  //   ],
+  //   // Format MarkDown and JSON
+  //   "**/*.(md|json)": (filenames) => `prettier --write ${filenames.join(" ")}`,
+};
